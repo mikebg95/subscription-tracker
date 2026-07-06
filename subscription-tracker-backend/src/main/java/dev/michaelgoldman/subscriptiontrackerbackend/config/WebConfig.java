5c1aeca29c307f6de8412e27c424af0c0390683c
Michael <mikebgoldman95@gmail.com>
2026-07-06 16:39:39 +0200

    (temporarily) turn of CORS safety by allowing all origins (for testing).

--- subscription-tracker-backend/src/main/java/dev/michaelgoldman/subscriptiontrackerbackend/config/WebConfig.java
+++ subscription-tracker-backend/src/main/java/dev/michaelgoldman/subscriptiontrackerbackend/config/WebConfig.java
@@ -3,6 +3,7 @@ package dev.michaelgoldman.subscriptiontrackerbackend.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.method.HandlerTypePredicate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,5 +14,14 @@ public class WebConfig implements WebMvcConfigurer {
         configurer.addPathPrefix("/api/v1",
                 HandlerTypePredicate.forAnnotation(RestController.class)
                         .and(HandlerTypePredicate.forBasePackage("org.springdoc").negate()));
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
